83df1aa981ea4bd224f3e5ce70053261fe3300bb
Eric19906913 <ericgaston1234@gmail.com>
2022-11-14 19:51:41 -0300

    feat: Initial commit

---
+++ ClinicHistoriesApp/ClinicHistories/LoginView.cs
@@ -0,0 +1,15 @@
+namespace ClinicHistories
+{
+    public partial class LoginView : Form
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("some message");
+        }
+    }
+}

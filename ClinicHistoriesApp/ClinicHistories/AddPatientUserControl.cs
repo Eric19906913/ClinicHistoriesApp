11b37da84c773b8dfc0c76a0230876e85456336c
Eric19906913 <ericgaston1234@gmail.com>
2022-11-17 21:51:58 -0300

    feat: add patient user control

---
+++ ClinicHistoriesApp/ClinicHistories/AddPatientUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicHistories
+{
+    public partial class AddPatientUserControl : UserControl
+    {
+        public AddPatientUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
